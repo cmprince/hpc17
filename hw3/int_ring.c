bca2a0d5bfef898cbaff98713979efe6de37c067
cmprince <cmp670@nyu.edu>
2017-03-28 15:29:21 -0400

    usage notes and loop argument

--- hw3/int_ring.c
+++ hw3/int_ring.c
@@ -4,26 +4,34 @@
  */
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include "mpi.h"
 
 int main( int argc, char *argv[])
 {
-    int rank, numprocs, tag, origin, destination;
+    int rank, R, N, tag, origin, destination;
     MPI_Status status;
   
     char hostname[1024];
     gethostname(hostname, 1024);
-  
+ 
+    if (argc!=2){
+        printf("Usage: mpirun -np n %s R\n"
+               "    R: number of processes\n"
+               "    N: number of loops of the MPI ring.\n", argv[0]);
+        exit(0);
+    }
+    else
+        N = atoi(argv[1]);
+
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
+    MPI_Comm_size(MPI_COMM_WORLD, &R);
 
     int message_out=0;
     int message_in=0;
     int counter=0;
     tag = 99;
-    int N =4;
-    int R=numprocs;
     int i;
 
     for (i=0; i<N; i++)
